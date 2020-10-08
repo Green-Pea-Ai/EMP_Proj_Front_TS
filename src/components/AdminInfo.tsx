10c67be2409d65fbbe280083eab0a418b5ef5f87
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-10-08 14:37:31 +0900

    201008 Thur 팀 프로젝트.
    브랜치 변경 테스트 커밋.

--- src/components/AdminInfo.tsx
+++ src/components/AdminInfo.tsx
@@ -4,6 +4,7 @@ import {PageTemplate} from './index'
 
 const AdminInfo = () => <PageTemplate> <section className="admin_info">
     <h1> [ 관리자 정보 페이지 ] </h1>
+    <h2> 브랜치 변경 테스트 커밋 </h2>
     <Analytics/>
 </section></PageTemplate>
 
