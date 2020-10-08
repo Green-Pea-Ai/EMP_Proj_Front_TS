510c06cac93b1aae7ce2f7c3ffc5e184d5f6b0c1
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-10-08 20:53:50 +0900

    201008 Thur 20:49, 팀 프로젝트.
    admin 페이지 작업 진행 중..
    삽질의 연속..

--- src/components/AdminInfo.tsx
+++ src/components/AdminInfo.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import Analytics from "../routes/Dashboards/Analytics";
-import {PageTemplate} from './index'
+import { PageTemplate } from './index'
+import GlobalStyles from './alpha_style/GlobalStyles';
 
-const AdminInfo = () => <PageTemplate> <section className="admin_info">
-    <h1> [ 관리자 정보 페이지 ] </h1>
-    <h2> 브랜치 변경 테스트 커밋 </h2>
-    <Analytics/>
-</section></PageTemplate>
+// import { useRoutes } from 'react-router-dom';
+// import DashboardLayout from "../layouts/DashboardLayout";
+import Budget from '../views/reports/dashboardView/Budget'
+import TotalCustomers from "../views/reports/dashboardView/TotalCustomers";
+import TasksProgress from "../views/reports/dashboardView/TasksProgress";
+import TotalProfit from "../views/reports/dashboardView/TotalProfit";
+import Sales from "../views/reports/dashboardView/Sales";
+import TrafficByDevice from "../views/reports/dashboardView/TrafficByDevice";
+import LatestProducts from "../views/reports/dashboardView/LatestProducts";
+import LatestOrders from "../views/reports/dashboardView/LatestOrders";
+
+
+const AdminInfo = () => {
+
+    
+    return ( 
+        <PageTemplate> <section className="admin_info">
+        <GlobalStyles/>
+        <h1> [ 관리자 정보 페이지 ] </h1>
+        
+        <Budget/>
+        <TotalCustomers/>
+        <TasksProgress/>
+        <TotalProfit/>
+        <Sales />
+        <TrafficByDevice/>
+        <LatestProducts/>
+        <LatestOrders/>
+
+        </section></PageTemplate>
+        
+    );
+};
 
 export default AdminInfo
