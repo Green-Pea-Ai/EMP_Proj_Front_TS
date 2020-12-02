27f0e671ee9ca8a48242c14b1ba10deb6ed3adea
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-12-02 20:35:19 +0900

    201202 Wed 20:34, 리뷰 부분 작성.

---
+++ src/modules/cop/rev/review/review.service.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { context as c } from '../../../context'
+
+export const reviewService = {
+    getReviews
+};
+
+async function getReviews(reviews) {
+    const req = {
+        method: c.get,
+        url: `${ c.url }/api/reviews/${ reviews }`,
+        // data: {cheese}
+    }
+    const resp = await axios(req)
+
+    const data = resp.data
+
+    alert('Connection is Successful !!')
+    return data
+}
